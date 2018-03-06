d7dd2fb53deb76ede4e47f072c169dd939745a37
github username <github email address>
2018-03-06 11:59:16 -0500

    set up api_communicator

---
+++ lib/api_communicator.rb
@@ -0,0 +1,34 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+def get_weather_from_api
+  #make the web request
+  weather_array = []
+  all_weather = RestClient.get('https://www.metaweather.com/api/location/2459115/')
+  weather_instance = JSON.parse(all_weather)
+
+  current_weather = weather_instance["consolidated_weather"][0]
+        # *****  a = (0..(amount of days they want)).to_a *******
+  #index [0..8] would return the upcoming week's forecast
+  #this is the single weather hash for one day at one location
+  weather_array << current_weather
+  #we add it to an array so that we have a colle}ction of "weathers"
+  #then we want to go through each one, modifying each hash to select the key/value
+  #pairs we want to keep
+  #we put this in an array called "to be saved"
+  to_be_saved = weather_array.collect do |current_weather_by_location|
+    current_weather_by_location.select do |k, v|
+      %w"weather_state_name min_temp max_temp the_temp wind_speed humidity".include? k
+    end
+  end
+
+  #go through our array of customized hashes
+  #on each one, instantiate a new object using its "attributes"
+  #and then save it
+  to_be_saved.each do |weather_hash|
+    new_object = Weather.new(weather_hash)
+    new_object.save
+  end
+
+end
---
+++ lib/command_line_interface.rb
@@ -0,0 +1,35 @@
+
+
+
+require "pry"
+def welcome
+  # puts out a welcome message here!
+  puts "Hello padawan, welcome to the grand library of Star Wars wisdom!"
+  puts "Please enter 'character' or 'movie'"
+end
+
+def get_movie_from_user
+  puts "Please enter a film"
+  # use gets to capture the user's input. This method should return that input, downcased.
+  info = gets.chomp
+  loop do
+    if info.is_a? String
+      return info.downcase
+    else
+      puts "Please enter a valid input!"
+    end
+  end
+end
+
+def get_character_from_user
+  puts "Please enter a character"
+  # use gets to capture the user's input. This method should return that input, downcased.
+  info = gets.chomp
+  loop do
+    if info.is_a? String
+      return info.downcase
+    else
+      puts "Please enter a valid input!"
+    end
+  end
+end
