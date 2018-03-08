42ef46eb1d3337c9247b23098b860a89d2422437
github username <github email address>
2018-03-08 14:32:47 -0500

    details

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -132,7 +132,7 @@ def switch_location(user)
       user.save
       # binding.pry
     else
-      puts "please enter a valid selection"
+      puts "Please enter a valid selection"
       switch_location(user)
     end
 end
@@ -183,67 +183,4 @@ selection = gets.chomp
       puts "Please enter a valid selection"
       switch(user)
     end
-end
-
-
-#user types "what is the temperature?"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# require "pry"
-def welcome
-  # puts out a welcome message here!
-  puts "Welcome message"
-end
-
-
-
-def get_movie_from_user
-  puts "Please enter a film"
-  # use gets to capture the user's input. This method should return that input, downcased.
-  user_id = gets.chomp
-  loop do
-    if info.is_a? String
-      return info.downcase
-    else
-      puts "Please enter a valid input!"
-    end
-  end
-end
-
-def get_character_from_user
-  puts "Please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
-  info = gets.chomp
-  loop do
-    if info.is_a? String
-      return info.downcase
-    else
-      puts "Please enter a valid input!"
-    end
-  end
 end
